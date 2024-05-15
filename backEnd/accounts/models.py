c33c7344bad76159556a2ac8cafc72e9aba16162
kihun <chae0738@naver.com>
2024-05-16 01:32:54 +0900

    PJT-17 초기세팅

---
+++ backEnd/accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+
+class User(AbstractUser):
+    followings = models.ManyToManyField(
+        'self', symmetrical=False, related_name='followers')
+    # email을 사용자 이름이자 unique한 값으로 회원가입을 받을 꺼임 eg> applehyunsoo@ssafy.com
+    username = models.EmailField(unique=True)
+    profile_pic = ProcessedImageField(
+        blank=True,
+        upload_to='profile/images',
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 90},
+    )
