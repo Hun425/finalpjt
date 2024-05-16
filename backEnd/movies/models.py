4920934befa2b97438001247aea0e96dec7f808a
Kihun <chae0738@naver.com>
2024-05-16 14:51:41 +0900

    PJT-18 DB 및 api 생성

--- backEnd/movies/models.py
+++ backEnd/movies/models.py
@@ -24,16 +24,14 @@ class Movie(models.Model):
     actors = models.ManyToManyField(Actor, related_name='movies')
     title = models.CharField(max_length=300)
     overview = models.TextField()
-    budget = models.BigIntegerField()
     popularity = models.FloatField()
     poster_path = models.TextField(null=True)
     release_date = models.DateField(null=True, default=datetime.date.today)
-    revenue = models.BigIntegerField()
     runtime = models.IntegerField(null=True)
     tagline = models.TextField(null=True)
     vote_average = models.FloatField(null=True)
     vote_count = models.IntegerField(null=True)
-    words = models.TextField(null=True)
+
     like_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name='like_movies'
     )
