3ecb79881be46fbfbb86c0f02dfa33a7eb5695b9
Kihun <chae0738@naver.com>
2024-05-20 17:51:17 +0900

    PJT-39 영화 목록 나이순

--- backEnd/movies/serializers.py
+++ backEnd/movies/serializers.py
@@ -62,7 +62,7 @@ class ReviewSerializer(serializers.ModelSerializer):
                 'updated_at',
             )
             read_only_fields = ('review',)
-    comments = CommentSerializer(many=True)
+    comments = CommentSerializer(many=True,read_only=True) # 필수 포함 아니니깐 read_only=True
 
 
 
